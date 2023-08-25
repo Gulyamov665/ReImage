942011857c90096adf9cf1c5677bfd37e9857198
unknown <gulyamov640@gmail.com>
2023-08-25 12:09:45 +0500

    change

--- multi_upload/models.py
+++ multi_upload/models.py
@@ -1,14 +1,14 @@
+import os
 from django.db import models
-from django_resized import ResizedImageField
 
 
 class Product(models.Model):
     CATEGORY_CHOICES = [
-                        ("toys", "toys"), 
-                        ("foods", "foods"),
-                        ("cosmetics", "cosmetics"), 
-                        ("fashion", "fashion")
-                        ]
+        ("toys", "toys"),
+        ("foods", "foods"),
+        ("cosmetics", "cosmetics"),
+        ("fashion", "fashion"),
+    ]
     name = models.CharField(max_length=50, blank=False)
     description = models.CharField(max_length=255, blank=False)
     category = models.CharField(max_length=9, choices=CATEGORY_CHOICES)
@@ -16,15 +16,20 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=7, decimal_places=2)
     date = models.DateField(auto_now_add=True)
 
-
     def __str__(self):
         return self.name
 
 
-class ProductsImage(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to="products")
+def user_directory_path(instance, filename):
+    return "{0}/{1}".format(instance.product.name, filename)
 
+
+class ProductsImage(models.Model):
+    name = models.CharField(max_length=200, blank=True, null=True)
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="images"
+    )
+    image = models.ImageField(upload_to=user_directory_path)
 
     def __str__(self):
         return self.product.name
--- multi_upload/views.py
+++ multi_upload/views.py
@@ -15,19 +15,25 @@ class ProductCreateView(CreateAPIView):
     serializer_class = ProductSerializers
 
 
-
 class ImageResizeView(APIView):
-    def post(self, request):
+    def post(self, request, n):
+        user = request.user
         res_width = 1500
-        input_folder = os.path.join(settings.BASE_DIR, 'media\products')
-        output_folder = os.path.join(settings.BASE_DIR, 'media\output')
+        input_folder = os.path.join(settings.MEDIA_ROOT, str(user.username))
+        output_folder = os.path.join(settings.MEDIA_ROOT, "output", str(user.username))
+        name = 0
 
         for file in os.listdir(input_folder):
+            name += 1
+            filename = str(name)
             if file.endswith(".jpg") or file.endswith(".png"):
                 img = Image.open(os.path.join(input_folder, file))
-                wpercent = (res_width / float(img.size[0]))
+                wpercent = res_width / float(img.size[0])
                 hsize = int((float(img.size[1]) * float(wpercent)))
                 img = img.resize((res_width, hsize), Image.Resampling.LANCZOS)
-                img.save(os.path.join(output_folder, file))
+                try:
+                    img.save(os.path.join(output_folder, filename + ".jpg"))
+                except OSError:
+                    img.save(os.path.join(output_folder, filename + ".png"))
+        return Response({"message": "Images resized successfully"})
 
-        return Response({'message': 'Images resized successfully'})
