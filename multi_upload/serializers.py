905d3299d6bc2e23ff085f171497b78dbb206610
Умид Бомуратов <antikko99@gmail.com>
2023-08-25 11:45:53 +0500

    api is ready

--- multi_upload/serializers.py
+++ multi_upload/serializers.py
@@ -1,34 +1,40 @@
 from rest_framework import serializers
 from PIL import Image
-from .models import Product, ProductsImage
+from .models import Images, Restaurant
+from django.contrib.auth.models import User
 
 
-class ProductsImageSerializers(serializers.ModelSerializer):
+class ImageSerializers(serializers.ModelSerializer):
     class Meta:
-        model = ProductsImage
+        model = Images
         fields = "__all__"
 
 
 class ProductSerializers(serializers.ModelSerializer):
-    images =  ProductsImageSerializers(many=True, read_only=True)
+    images =  ImageSerializers(many=True, read_only=True)
     uploaded_images = serializers.ListField(
         child=serializers.ImageField(allow_empty_file=False, use_url=False),
         write_only=True
     )
 
     class Meta:
-        model = Product
-        fields = ["id", "name", "description", "price", "date", "images",
+        model = Restaurant
+        fields = ["id", "user" ,"name", "description", "price", "images",
                   "uploaded_images"]
 
     def create(self, validated_data):
         uploaded_images = validated_data.pop("uploaded_images")
-        product = Product.objects.create(**validated_data)
+        product = Restaurant.objects.create(**validated_data)
 
         for image in uploaded_images:
             
 
 
-            ProductsImage.objects.create(product=product, image=image)
+            Images.objects.create(product=product, image=image)
 
         return product
+    
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["username", "email","password" ]
--- multi_upload/views.py
+++ multi_upload/views.py
@@ -1,13 +1,14 @@
 import os
 from PIL import Image
 from rest_framework.response import Response
-from django.shortcuts import render
-from rest_framework.generics import CreateAPIView
+from rest_framework.decorators import api_view
+from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.views import APIView
 from django.conf import settings
 from rest_framework.parsers import MultiPartParser, FormParser
-from multi_upload.models import Product
-from multi_upload.serializers import ProductSerializers
+from multi_upload.models import Images, Restaurant
+from multi_upload.serializers import ProductSerializers, UserSerializer
+from django.contrib.auth.models import User
 
 
 class ProductCreateView(CreateAPIView):
@@ -15,12 +16,15 @@ class ProductCreateView(CreateAPIView):
     serializer_class = ProductSerializers
 
 
-
 class ImageResizeView(APIView):
     def post(self, request):
+        user = request.user
         res_width = 1500
-        input_folder = os.path.join(settings.BASE_DIR, 'media\products')
-        output_folder = os.path.join(settings.BASE_DIR, 'media\output')
+        input_folder = os.path.join(settings.MEDIA_ROOT, str(user.username))
+        output_folder = os.path.join(settings.MEDIA_ROOT,'output', str(user.username))
+
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
 
         for file in os.listdir(input_folder):
             if file.endswith(".jpg") or file.endswith(".png"):
@@ -31,3 +35,43 @@ class ImageResizeView(APIView):
                 img.save(os.path.join(output_folder, file))
 
         return Response({'message': 'Images resized successfully'})
+    
+
+class DetailUserApiView(ListAPIView):
+    parser_class = [MultiPartParser, FormParser]
+    serializer_class = ProductSerializers
+
+    def get_queryset(self):
+        user = self.kwargs['user_id']
+        queryset = Restaurant.objects.filter(user_id=user)
+        return queryset
+    
+
+class DetailAllApiView(ListAPIView):
+    parser_class = [MultiPartParser, FormParser]
+    queryset = Restaurant.objects.all()
+    serializer_class = ProductSerializers
+
+
+
+class UserCreateAPI(CreateAPIView):
+    parser_class = [MultiPartParser, FormParser]
+    serializer_class = UserSerializer
+
+
+class DetailUserApiView(ListAPIView):
+    parser_class = [MultiPartParser, FormParser]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+    'Create' : 'api/create',
+    'Detail/user' : 'api/detail/<int:user_id>',
+    'Detail/all' : 'api/detail',
+    'Create User' : 'api/create_user',
+    'Resize' : 'api/resize_image',
+    }
+    return Response(api_urls)
